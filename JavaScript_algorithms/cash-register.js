62997b82d21326c28bd26d0e079451bb3535ebe4
Marla Malone <marla.r.malone@gmail.com>
2022-02-10 20:31:57 -0600

    Update cash-register.js
    It works!

--- JavaScript_algorithms/cash-register.js
+++ JavaScript_algorithms/cash-register.js
@@ -1,54 +1,73 @@
 function checkCashRegister(price, cash, cid) {
-  
-  let statMap = new Map();
-  let statCases = "";
+  let drawerStatus = "";
   let change = cash - price;
-  console.log(change);
-
+  let resultArr = [];
+  //console.log(change);
+  let changeArr = [];
+  let numVal = [["PENNY", .01], ["NICKEL", .05], ["DIME", .1], ["QUARTER", .25], ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED", 100]];
   //Get the total amount of change in drawer
   let cidTotal = 0;
-  for (let i = 0; i < cid.length; i++) {
+  for (let i = 0; i <= 8; i++) {
     cidTotal += cid[i][1];
   }
+  //console.log(cidTotal);
   cidTotal = Math.round(cidTotal * 100) / 100;
-  console.log(cidTotal);
-
-  let changeArr = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
   if (change == cidTotal) {
-    statCases = "CLOSED";
+    drawerStatus = "CLOSED";
+    changeArr.push(...cid);
   }
 
-  if (change >= cidTotal) {
-    statCases = "INSUFFICIENT_FUNDS";
+  if (change > cidTotal) {
+    drawerStatus = "INSUFFICIENT_FUNDS";
   }
 
   if (change < cidTotal) {
+    drawerStatus = "OPEN";
+    let temp;
     
-    statCases = "OPEN";
+    for (let i = 8; i >= 0; i--) {
+      //If the amount of change left is bigger than the currency being evaluated
+      if (change > numVal[i][1]) {
+        //Temporary variable shows the max amount that could be returned from currency type (like TWENTY)
+        temp = (Math.floor(change / numVal[i][1])) * numVal[i][1];
+        //console.log(temp);
+        //If the max amount that could be returned is more than what is in the drawer
+        if (temp >= cid[i][1]) {
+          //Get difference between temp and what's in the drawer
+          let temp2 = temp - cid[i][1];
+          //Set temp to the difference between temp and the amount that isn't available (temp2)
+          temp -= temp2;
+          //Add currency type and temp to change array
+          changeArr.push([numVal[i][0], temp]);
+        }
+        //If there is more money in the drawer than temp amount
+        if (temp < cid[i][1]) {
+          //console.log(temp);
+          //Add currency type and temp to change array
+          changeArr.push([numVal[i][0], temp]);
+        }
+        //Calculate updated amount of change
+        change -= temp;
+        //Round change to the tenth decimal place
+        change = Math.round((change + Number.EPSILON) * 100) / 100
+        //console.log(change);
+      } 
+      if (change > 0 && i == 0) {
+        drawerStatus = "INSUFFICIENT_FUNDS";
+        changeArr = [];
+      }
+    }
 
-    changeArr[8][1] = change - cid[8][1];
-    
   }
 
-  //Switch cases that will set statMap keys and values
-  //to be returned at the end.
-  switch (statCases) {
-    case "INSUFFICIENT_FUNDS":
-      statMap.set('status', "INSUFFICIENT_FUNDS");
-      statMap.set('change', "[]");
-      break;
-    case "CLOSED":
-      statMap.set('status', "CLOSED");
-      statMap.set('change', cidTotal);
-      break;
-    case "OPEN":
-      statMap.set('status', "OPEN");
-      statMap.set('change', changeArr.map(changeArr));
-      break;
-  }
+  resultArr["status"] = drawerStatus;
+  resultArr["change"] = changeArr;
+  console.log(resultArr);
   
-  console.log(statMap);
-  //return statArr;
+  let resultObj = {...resultArr};
+  
+  console.log(resultObj);
+  return resultObj;
 }
 
-checkCashRegister(19.5, 120, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
